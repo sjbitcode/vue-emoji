d4b6b569a5ad22adc1904e2d06eccae1dd302e64
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-12 19:52:08 -0500

    Load images dynamically with require

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>{{ greeting }}</h1>
+
+    <!-- Image examples
+    <img src="./assets/img/u1F60D.png">
+    <img v-bind:src="require('./assets/img/u1F60D.png')">
+    -->
+
+    <img v-for="name in image_names" v-bind:src="imgUrl(name)">
+
+
   </div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      greeting: 'Hello, there!',
+
+      image_names: [
+        'u1F60D',
+        'u1F60E',
+        'u1F60F'
+      ]
+    }
+  },
+
+  methods: {
+    imgUrl: function (name) {
+      return require('./assets/img/' + name + '.png');
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
