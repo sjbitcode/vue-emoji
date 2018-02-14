71328cff60e045d21b26dbb6032e756c18414aa3
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-14 17:52:53 -0500

    Add vue clipboard, add emoji code examples in html & JS

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <h1>{{ greeting }}</h1>
 
     <!-- Image examples
     <img src="./assets/img/u1F60D.png">
     <img v-bind:src="require('./assets/img/u1F60D.png')">
+    <img v-bind:src="require(`${path}`)">
     -->
 
     <img v-for="name in image_names" v-bind:src="imgUrl(name)">
 
-    <img v-bind:src="require(`${image}`)">
+    <div>
+      <h1 @click="copy">{{ greeting }}</h1>
+    </div>
+
+    <p style="font-size:5rem;width:100%;">&#x1F354</p>
+    
+    <p style="font-size:5rem;width:100%;">&#x1F486;&#x1F3FB;&#x200D;&#x2640;&#xFE0F;</p>
+    
+    <p style="font-size:5rem;width:100%;">&#x1F3CC;&#x1F3FF;&#x0200D;&#x02640;&#x0FE0F;</p>
 
 
   </div>
@@ -23,8 +31,6 @@ export default {
     return {
       greeting: 'Hello, there!',
 
-      image: './assets/img/u1F60D.png',
-
       image_names: [
         'u1F60D',
         'u1F60E',
@@ -36,11 +42,21 @@ export default {
   methods: {
     imgUrl: function (name) {
       return require('./assets/img/' + name + '.png');
+    },
+
+    copy: function() {
+      // this.$clipboard(String.fromCodePoint(0x1F354));
+      this.$clipboard(String.fromCodePoint(0x1F486,0x1F3FB,0x200D,0x2640,0xFE0F));
     }
   }
 }
 </script>
 
 <style>
-
+h1 {
+  padding: 10px;
+  border: 2px solid black;
+  width: 100px;
+  background-color: #ffe;
+}
 </style>
