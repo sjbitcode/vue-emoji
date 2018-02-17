dadfa57e39fdc27651dcb7b0421d2cdb64337016
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-16 19:16:01 -0500

    emoji codes

---
+++ emoji_codes/emoji_notes.py
@@ -0,0 +1,17 @@
+skin_tones: {
+	'light_skin_tone':        u'\U0001F3FB',
+	'medium_light_skin_tone': u'\U0001F3FC',
+	'medium_skin_tone':       u'\U0001F3FD',
+	'medium_dark_skin_tone':  u'\U0001F3FE',
+	'dark_skin_tone':         u'\U0001F3FF'
+}
+
+gender: {
+	'female_sign': u'\U00002640',
+	'male_sign':   u'\U00002642'
+}
+
+misc : {
+	'zero_width_joiner':  u'\U0000200D',
+	'variation_selector': u'\U0000FE0F'
+}
