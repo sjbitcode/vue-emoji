abe3d4b1c6a51a4ce915f624442ba9de7e4d8362
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-18 16:56:14 -0500

    Added tooltip for emojis

--- src/components/Emoji.vue
+++ src/components/Emoji.vue
@@ -1,42 +1,70 @@
 <template>
-	<div>
-		<div v-for="emoji_category in emojis">
-			<div v-for="val, key in emoji_category">
-				<span @click="copy(key)">{{ key }}</span>
-				<p v-show="val">{{ val }}</p>
-			</div>
-		</div>
-	</div>
+    <div id="emojis">
+        
+        <div id="category" v-for="emoji_category, emoji_category_text in emojis">
+            
+            <h1>{{ emoji_category_text }}</h1>
+            
+            <div id="emoji" v-for="val, key in emoji_category">
+                <span @click="copy(key)" v-tooltip.top="{content: val, delay: 0}">{{ key }}</span>
+            </div>
+            
+        </div>
+        
+    </div>
 </template>
 
 
 <script>
-	export default {
-		props: {
-			emojis: {
-				type: Object,
-				required: true
-			}
-		},
+    export default {
+        props: {
+            emojis: {
+                type: Object,
+                required: true
+            }
+        },
 
-		data() {
-			return {
-				
-			}
-		},
+        data() {
+            return {
+                
+            }
+        },
 
-		methods: {
-			copy: function(emoji) {
-		      this.$clipboard(emoji);
-		    }
-		}
-	}
+        methods: {
+            copy: function(emoji) {
+              this.$clipboard(emoji);
+            },
+
+            emoji_alias: function(alias) {
+                return alias;
+            }
+        }
+    }
 </script>
 
 
 <style>
-	span {
-		font-size:5rem;
-		width:100%;
-	}
+
+    #emojis {
+        width: 100%;
+        max-width: 1200px;
+        margin: 40px auto;
+        padding: 0 20px;
+        box-sizing: border-box;
+    }
+
+    #category {
+        display: flex;
+        flex-wrap: wrap;
+        list-style-type: none;
+        padding: 0;
+    }
+
+    #emoji {
+        flex-grow: 1;
+        text-align: center;
+        padding: 20px;
+        margin: 10px;
+        font-size:5rem;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Clipboard from 'v-clipboard'
+import Tooltip from 'vue-directive-tooltip';
+import "../node_modules/vue-directive-tooltip/css/index.css";
+
 
 Vue.use(Clipboard);
+Vue.use(Tooltip);
 
 new Vue({
   el: '#app',
