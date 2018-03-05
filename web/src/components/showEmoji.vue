1de8838f3794e45e9b1f08039f22414568e2bf2c
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-03-04 20:59:45 -0500

    Add buefy tooltip. Remove previous tooltip plugin

--- web/src/components/showEmoji.vue
+++ web/src/components/showEmoji.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="container is-fluid">
+
+    <transition name="slither">  
+        <div>
+            &#x1F496;
+        </div>
+    </transition>
             
     <div class="columns is-multiline is-mobile">
         <div v-for="emoji in emojis">
-            <div class="emoji column" v-on:click="copy(emoji.shortcode)" :ref="'emoji_' + emoji.shortcode">
-                {{ emoji.surrogate_pairs | format-code }}
-            </div>
+
+            <b-tooltip class="my-tooltip" :label="tooltipText(emoji)">
+                <div class="emoji column" v-on:click="copy(emoji.shortcode)" :ref="'emoji_' + emoji.shortcode">
+                    <a>
+                        {{ emoji.surrogate_pairs | format-code }}
+                    </a>
+                </div>
+            </b-tooltip>
         </div>
     </div>
 
@@ -40,6 +51,15 @@
               var ref_name = 'emoji_' + shortcode;
               this.$clipboard(this.$refs[ref_name][0].innerText);
             },
+
+            formatShortcode: function(str) {
+                return ':' + str + ':'
+            },
+
+            tooltipText: function(emoji) {
+                var text = this.formatShortcode(emoji.shortcode) + '\n' + emoji.shortened_codepoint;
+                return text;
+            }
         }
     }
 </script>
@@ -48,5 +68,17 @@
 .emoji {
     font-size: 6em;
     margin: 0 30px;
+}
+
+.slither-enter-active, .slither-leave-active {
+  transition: transform 3s;
+}
+
+.slither-enter, .slither-leave-to {
+  transform: translateX(-100%);
+}
+
+.slither-enter-to, .slither-leave {
+  transform: translateX(0);
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -2,15 +2,12 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 import Clipboard from 'v-clipboard'
-import Tooltip from 'vue-directive-tooltip';
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
-import "../node_modules/vue-directive-tooltip/css/index.css";
 
 
 Vue.use(VueResource)
 Vue.use(Clipboard);
-Vue.use(Tooltip);
 Vue.use(Buefy, {
 	defaultIconPack: 'fab'
 });
