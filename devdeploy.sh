f68610c96cf6ab1d3d6fa792ed1adf696520263e
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-03-28 17:58:54 -0400

    Reformat, clean up compose files. Add comments to bash deploy scripts

--- devdeploy.sh
+++ devdeploy.sh
@@ -1,29 +1,32 @@
-# Step 1 - Pull new images
+# Step 1 - Delete logs directory
 # Step 2 - Stop all containers
 # Step 3 - Remove all containers
-# Step 4 - Remove web volume
-# Step 5 - Start docker-compose service
+# Step 4 - Build images
+# Step 5 - Remove webdata and appstatic volumes
+# Step 6 - Start docker-compose service
 
-# dc pull && dc stop -t 1 && dc rm -fv && docker volume rm webdata && docker volume create --name=webdata && dc up -d
 
+# Delete logs directory
 echo -e "\nDeleting logs directory..."
 rm -rf logs
 
+# Stop all containers
 echo -e "\nRunning dc stop -t 1..."
 docker-compose stop -t 1
 
+# Remove all containers
 echo -e "\nRunning dc rm -fv..."
 docker-compose rm -fv
 
+# Build images
 echo -e "\nRunning dc build..."
 docker-compose build
 
+# Remove webdata and appstatic volumes
 echo -e "\nRunning docker volume rm webdata, appstatic..."
 docker volume rm webdata
+docker volume rm appstatic
 
-echo -e "\nRunning docker volume create --name=webdata..."
-docker volume create --name=webdata
-docker volume create --name=appstatic
-
+# Start docker-compose service
 echo -e "\nRunning dc up -d..."
 docker-compose up -d
--- stagingdeploy.sh
+++ stagingdeploy.sh
@@ -1,29 +1,36 @@
-# Step 1 - Pull new images
+# Step 1 - Delete logs directory
 # Step 2 - Stop all containers
 # Step 3 - Remove all containers
-# Step 4 - Remove web volume
-# Step 5 - Start docker-compose service
+# Step 4 - Build images
+# Step 5 - Remove web volume
+# Step 6 - Create web and app volume
+# Step 7 - Start docker-compose service
 
-# dc pull && dc stop -t 1 && dc rm -fv && docker volume rm webdata && docker volume create --name=webdata && dc up -d
-
+# Delete logs directory
 echo -e "\nDeleting logs directory..."
 rm -rf logs
 
+# Stop all containers
 echo -e "\nRunning dc stop -t 1..."
 docker-compose -f docker-compose-staging.yml stop -t 1
 
+# Remove all containers
 echo -e "\nRunning dc rm -fv..."
 docker-compose -f docker-compose-staging.yml rm -fv
 
+# Build images
 echo -e "\nRunning dc build..."
 docker-compose -f docker-compose-staging.yml build
 
+# Remove web volume
 echo -e "\nRunning docker volume rm webdata, appstatic..."
 docker volume rm webdata
 
+# Create web and app volume
 echo -e "\nRunning docker volume create --name=webdata..."
 docker volume create --name=webdata
 docker volume create --name=appstatic
 
+# Start docker-compose service
 echo -e "\nRunning dc up -d..."
 docker-compose -f docker-compose-staging.yml up -d
