f6e8a91239f749d3f9e4f0f97f2dd9949adc0c4e
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-03-29 21:36:21 -0400

    Uncomment stuff in deploy/build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,20 +4,23 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 ROOT_PATH=$( cd "$(dirname "${PARENT_PATH}")" ; pwd -P )
 
+# Bundle frontend app
 echo -e "-- Running npm run build \n --"
+
 WEB_DIR="$ROOT_PATH/web"
 npm --prefix $WEB_DIR run build
 
-# echo -e "-- Building application containers --\n"
 
-# # Build docker images from source code.
-# if [[ $1 = "full" ]] ; then
-#     docker-compose -f docker-compose-staging.yml build --no-cache
-# else
-#     docker-compose -f docker-compose-staging.yml build
-# fi
+# Build docker images from source code.
+echo -e "-- Building application containers --\n"
 
-# # Push docker images to dockerhub registry.
-# docker-compose push
+if [[ $1 = "full" ]] ; then
+    docker-compose -f docker-compose-staging.yml build --no-cache
+else
+    docker-compose -f docker-compose-staging.yml build
+fi
 
-# echo -e "\n-- Application containers have been built and pushed --"
+# Push docker images to dockerhub registry.
+docker-compose push
+
+echo -e "\n-- Application containers have been built and pushed --"
